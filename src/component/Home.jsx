e3a8a318f931eaea8b48c562dfb5fceb9cefc48d
Muzz <creative.joomdev@gmail.com>
2021-05-27 17:12:40 +0500

    home page dynamic

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,15 +1,10 @@
 import React, { Component } from 'react'
-import { list } from '../helper/api'
 import axios from 'axios'
+import rightOffer from "../assets/right-offer.png";
+import { Row, Col, Image } from "react-bootstrap";
 
 export default class Header extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     data: [],
-  //     offset: 0,
-  //   };
-  // }
+  
 
   state = {
     data: [],
@@ -27,7 +22,7 @@ export default class Header extends Component {
    getData=()=> {
     axios
       .get(
-        `https://api.mukify.com/graphiql/?query=query{localizedFlatItem(id:%20%22%22searchString: "", offset: ${this.state.offset}, first:20){edges{node{id
+        `https://api.mukify.com/graphiql/?query=query{localizedFlatItem(id:%20%22%22searchString: "", offset: ${this.state.offset}, first:8){edges{node{id
         basicInfo {
           name
           shortDescription
@@ -62,15 +57,13 @@ export default class Header extends Component {
   }
 
   loadMore = ()=> {
-    console.log(window.innerHeight + document.documentElement.scrollTop);
-    console.log(document.scrollingElement.scrollHeight);
     if (
       Math.ceil(window.innerHeight + document.documentElement.scrollTop) ===
       document.scrollingElement.scrollHeight
     ) {
       axios
         .get(
-          `https://api.mukify.com/graphiql/?query=query{localizedFlatItem(id:%20%22%22searchString: "", offset: ${this?.state?.offset ? this.state.offset : 1}, first:20){edges{node{id
+          `https://api.mukify.com/graphiql/?query=query{localizedFlatItem(id:%20%22%22searchString: "", offset: ${this?.state?.offset ? this.state.offset : 1}, first:8){edges{node{id
         basicInfo {
           name
           shortDescription
@@ -108,15 +101,7 @@ export default class Header extends Component {
     }
     
   }
-  handleScroll() {
-    if (
-      window.innerHeight + document.documentElement.scrollTop !==
-      document.documentElement.offsetHeight
-    )
-      return this.loadMore();
-    console.log("Fetch more list items!");
-  }
-
+  
   componentDidMount() {
     this.getData();
   }
@@ -125,33 +110,49 @@ export default class Header extends Component {
     const { data, offset } = this.state;
     console.log(data, offset);
     return (
-      <div className="row">
-        {data.map((nod) => {
-          return (
-            <div
-              className="col-md-3 col-sm-6 col-xm-1 col-lg-3"
-              key={nod?.node?.id}
-            >
-              <img
-                className="card-img-top"
-                src={nod?.node?.itemImagesSet[0]?.mediumThumbUrl}
-                alt="Card image cap"
-              />
-              <div className="card-body">
-                <small className="text-muted">
-                  {nod?.node?.basicInfo?.name}
-                </small>
-                <h5 className="card-title">
+      <>
+        <Row>
+          {data.map((nod) => {
+            return (
+              <Col xs={12} lg={3} md={3} sm={6} key={nod.node.id}>
+                <Image
+                  className="product-image"
+                  src={nod?.node?.itemImagesSet[0]?.mediumThumbUrl}
+                />
+                <p className="product-text">
                   {nod?.node?.basicInfo?.shortDescription}
+                </p>
+                <p className="product-subtext ">{nod?.node?.basicInfo?.name}</p>
+                <p className="product-price">
+                  {nod.node.additionalInfo[0].rows[0].columns[0]}
+                </p>
+              </Col>
+            );
+          })}
+          <div className="load" onScroll={() => this.handleScroll()}></div>
+        </Row>
+        {/* {data.length >= 8 && (
+          <div className="products-offers container mt-5 mb-5 col-sm-12">
+            <div className="row">
+              <div className="left-offer mt-5 mb-5 pt-5 pb-5 pl-5 col-sm-6">
+                <h5 style={{ fontWeight: "800" }}>Tulossa!</h5>
+                <h2 style={{ fontWeight: "400" }}>
+                  Tunnista muumimukit suoraan valokuvasta!
+                </h2>
+                <h5 className="pb-3" style={{ fontWeight: "600" }}>
+                  Aloita kokoelmasi kerääminen valokuvaamalla olemassa olevat
+                  aarteesi.
                 </h5>
-
-                <p className="card-text"></p>
+                <button>Kokeile</button>
               </div>
+              <div
+                className="right-offer mt-5 mb-5 col-sm-5"
+                style={{ backgroundImage: `url(${rightOffer})` }}
+              ></div>
             </div>
-          );
-        })}
-        <div className="load" onScroll={() => this.handleScroll()}></div>
-      </div>
+          </div>
+        )} */}
+      </>
     );
   }
 }
