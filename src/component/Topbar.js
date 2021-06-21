919713fa142828b06d33f1a9253e9db7a13a215e
Taimoor Khan <taimoor36372@gmail.com>
2021-06-21 07:17:35 -0700

    fix footer and naav bar for mobile

--- src/component/Topbar.js
+++ src/component/Topbar.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'; 
 import '../styling/style.scss'
 import { Nav, Navbar, NavDropdown, Row } from "react-bootstrap";
 import logo from '../assets/logo.svg'
-import {FaSearch} from "react-icons/fa";
+import {FaSearch,FaUser, FaChevronLeft} from "react-icons/fa";
 import Camera from '../assets/camera.png'
 
+
 export default function Topbar() {
+    const [user, setUser] = useState(false);  
+
+    const dropDownmenu = (event) => {
+      event.preventDefault()
+         setUser(!user)
+    }
+
     return (
         <>
         {/* <div className="top-bar pt-1">
@@ -38,9 +46,16 @@ export default function Topbar() {
         </div>
         <Navbar className="navbar-custom pt-5 col-md-12 col-sm-12 col-xs-12" style={{position:"fixed",top:"0",zIndex:"10",backgroundColor:"rgba(255,255,255,1)",}} collapseOnSelect expand="lg">
             <Navbar.Brand className="custom-width col-md-3 col-sm-3 col-xs-3">
-        <div className="header container-fluid ">
+        <div className="header container-fluid mr-2">
             <div className="logo">
-                <img src={logo} alt="mukify"></img>
+            <span className="mr-1 FaChevronLeftDisplayOnLargeScreen">
+                    <FaChevronLeft  color="black"/>  
+            </span>
+            <span className="mr-1 FaChevronLeftDisplaySmallScreen">
+                    <FaChevronLeft size="14px" color="black"/>  
+            </span>
+        
+                <img className="applyOnXsScreen" src={logo} alt="mukify"></img>
             </div>
             </div>
             </Navbar.Brand>
@@ -48,8 +63,8 @@ export default function Topbar() {
         <Navbar.Collapse className="justify" id="responsive-navbar-nav">
         <div className="tabs">
         <span className="search-area">
-            <a className="search" href=""><FaSearch /></a>
-            <a className="border-left" href=""><img className="" src={Camera} alt="loading"></img></a>
+            <a className="search" href=""><FaSearch color="black" /></a>
+            <a className="" style={{border:"none"}} href=""><img className="" src={Camera} alt="loading"></img></a>
             </span>
             <a className="pl-4" href="">Kirjaudu sisään</a>
         
@@ -58,8 +73,28 @@ export default function Topbar() {
         <button className="tab-btn ml-2">Rekisteröidy</button>
         </div>
         </Navbar.Collapse>
+        <div>
+            {/* mobile topbar */}
+        <div className="tabs-mob">
+        <span className="search-area">
+            <a className="search" href=""><FaSearch  color="black"/></a>
+            <a className="border-left" href=""><img style={{border:"none"}} className="" src={Camera} alt="loading"></img></a>
+            <a className="userIcon" href="" 
+            onClick={dropDownmenu}
+            ><FaUser color="black"/></a>
+            </span>
+           
+        </div>
+       
+        </div>
+        <div style={{marginTop:"18px"}} className={`dropdownPosition  ${user ? "openUser" : "closeUser"}`}>
+        <p className="" href="">Kirjaudu sisään</p>
+        <button className="tab-btnSmall ml-2">Rekisteröidy</button>
+        </div>
+        {/* mobile topbar */}
         
         </Navbar>
+        
         </>
     )
 }
